ad5f628fd46498b0bd49b18efdac83833518a0f3
ZSully09 <zsullivan93@gmail.com>
2019-10-23 22:31:24 +0000

    working output

--- input.js
+++ input.js
@@ -1,6 +1,11 @@
-function setupInput() {
+// Stores the active TCP connection object.
+let connection;
+
+function setupInput(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
+  stdin.on("data", movementInput);
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -12,6 +17,22 @@ function handleUserInput(key) {
     process.exit();
   }
 }
-setupInput();
 
+function movementInput(key) {
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+}
+
+// setupInput();
+module.exports = { connection };
 module.exports = { setupInput };
